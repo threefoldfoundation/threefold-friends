a10e538dd3ebbc667de4ec2272d3c868ac7d74aa
samaradel <samar@incubaid.com>
2020-06-08 21:10:35 +0200

    init value page #5

--- src/frontend/src/routes/Values.svelte
+++ src/frontend/src/routes/Values.svelte
@@ -1,4 +1,3 @@
-
 <!-- Wrapper -->
 <div id="wrapper">
 
@@ -6,74 +5,297 @@
   <div id="main">
 
     <!-- Post -->
-    <article class="container font-weight-normal">
-      <div class="row">
-      <header class="text-center mx-auto mb-2">
-        <div class="title">
-          <h1 class="text-uppercase my-3">conscious internet alliance</h1>
-          <p class="font-weight-normal h3">
-            A collective and open ecosystem of planet and human centric
-            projects
-          </p>
-        </div>
-      </header>
-        <span class="image featured">
-          <img src="images/green-header2.png" alt="" />
-        </span>
-        <div class="col-sm-10 offset-sm-1">
+    <article class="container ">
+      <span class="image featured">
+        <img src="images/green-header2.png" alt="" />
+      </span>
 
-          <p class="home_tf">
-            We are
-            <a href="http://threefold.io/" target="_blank">ThreeFold</a>
-            and we have built a bridge to a new peer-to-peer internet. Internet
-            that empowers equality, autonomy and sustainability.
-          </p>
-          <p class="home_tf">
-            As we launch the
-            <a href="https://threefold.io/capacity.html" target="_blank">
-              Grid
-            </a>
-            and other experiences on top, the time has come for us to cross over
-            to the decentralized world of peer-to-peer.
-          </p>
+      <p class="lead">
+        A collaborative ecosystem of projects and ambassadors that are expanding
+        and distributing social and environmental goods in a decentralized, peer
+        to peer manner for humanity and our planet
+      </p>
+      <p class="home_tf font-weight-bold">
+        We aim to empower +1 Billion people with sustainable access to
+        complementary education, internet, and currencies (banking) for less
+        than $1 per month.
+      </p>
 
-          <p class="home_tf">
-            A decentralized peer-to-peer world would be incomplete without
-            <a href="https://threefold.io/token.html" target="_blank">
-              ThreeFold Token
-            </a>
-            - a purpose driven decentralized digital currency serving as a fair
-            and trustworthy medium of exchange.
-          </p>
+      <div class="intro">
+        <div class="row">
+          <div class="col-sm-6">
+            <h3 class="my-3">Unlimited access to education</h3>
+            <p>
+              Education is empowerment. We focus on education content as well as
+              the required tools to provide education to a billion people.
+              Through simple technologies like video conferencing, and the
+              infrastructure technology from ThreeFold, we connect students with
+              educators around the world.
+            </p>
+          </div>
 
-          <p class="home_tf">
-            A decentralized peer-to-peer world would be impossible without the
-            <a
-              href="https://www.consciousinternet.org/#/circles"
-              target="_blank">
-              Threefold Ambassadors
-            </a>
-            , an ecosystem of beautifully aligned projects co-creating it.
-          </p>
+          <div class="col-sm-6">
+            <h3 class="my-3">Complementary digital currencies</h3>
+            <p>
+              Our Digital Nation needs a set of safe complementary digital
+              currencies. They need to be compatible with our existing fiat
+              currencies, maintain their value in all situations, globally
+              usable, freely exchangeable and cannot be abused by centralized
+              organizations or hackers.
+            </p>
+          </div>
 
-          <p class="home_tf">
-            With all the underlying elements, together we are co-creating the
-            new peer-to-peer internet, empowering a better future for humanity
-            and our planet.
-          </p>
-          <p class="home_tf text-center">
-            If not
-            <span class="home_span">us</span>
-            , who? If not
-            <span class="home_span">now</span>
-            , when?
-          </p>
-          <div class="text-center link_wrapper my-4">
-            <a href="#/circles" class="button large text-uppercase">
-              see projects
-            </a>
+          <div class="col-sm-6">
+            <h3 class="my-3">Connectivity, Internet and information</h3>
+            <br />
+            <p>
+              We need a safe and cost effective new Internet. The current one is
+              too expensive, power hungry, centralized and not scalable enough.
+              We need a better 100% decentralized alternative owned by us all.
+            </p>
+          </div>
+
+          <div class="col-sm-6">
+            <h3 class="my-3">
+              Our "Digital Self" needs to be owned by our selves
+            </h3>
+            <p>
+              We are cyborgs – our phones are an extension of our minds, they
+              make us smarter and faster. Our identity and data on the Internet
+              is our "Digital Self," which we unfortunately don't own. We became
+              a product and need our freedom back.
+            </p>
           </div>
         </div>
+      </div>
+
+      <!-- values -->
+      <div class="value my-5 p-5">
+        <div class="row">
+          <div class="col">
+            <h2 class="h2 text-center mx-auto text-uppercase font-weight-bold">
+              values
+            </h2>
+            <p class="lead text-center">
+              We, the citizens of FreeFlow Nation, believe that the world needs
+              a new nation - a global complementary Digital Nation.
+              <br />
+              A nation to complement not replace our existing nations, a nation
+              which has no physical boundaries and which welcomes everyone as a
+              digital citizen.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- FreeFlow Rights -->
+      <div class="ff-rights my-5">
+        <h2 class="h2 mx-auto text-uppercase my-4 font-weight-bold">
+          FreeFlow Rights
+        </h2>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            Everyone has access to affordable education, Internet, healthcare
+            and financial systems
+          </h3>
+          <p class="home_tf">
+            Through education, people can improve the living standards for
+            themselves and for their children. Education is the gateway to a
+            better life. For more than 3 billion people in the world today above
+            mentioned digital commodities are not available nor affordable. Cost
+            of the information system needs to be lower.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">Everyone has equal chances</h3>
+          <p class="home_tf">
+            Everyone has equal chances. Independent of location, race, gender,
+            religion, etc. leaving no one behind. Everyone can deliver services
+            or products to everyone in all transparency and fairness.
+          </p>
+        </div>
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            Everyone has the right to a safe, healthy, and secure environment
+          </h3>
+          <p class="home_tf">
+            Everyone has the right to a safe, healthy, and secure environment
+            Safety and health are human rights. Feeling safe is crutial for our
+            well-being as individuals. Our money and personal information needs
+            to be protected. Systems need to be safe & healthy (e.g. current 5G
+            is potentially harmful).
+          </p>
+        </div>
+      </div>
+
+      <!-- FreeFlow Declaration -->
+      <div class="ff-dec my-5">
+        <h2 class="h2 mx-auto text-uppercase my-3 font-weight-bold">
+          FreeFlow Declaration
+        </h2>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            We declare that we have power and we need to act.
+          </h3>
+          <p class="home_tf">
+            The world needs healing. There is too much talk and not enough
+            action. We believe that we have the power. We shouldn’t blame others
+            – governments, society, politicians or other countries. We all are
+            at the source of the problems the world is facing today, and
+            together we can do something about it.
+            <br />
+            We have a responsibility to improve the things we can have an impact
+            on instead of blaming others around us. This doesn’t mean we have no
+            right to a financially comfortable life, it means we have to act and
+            help heal the world. If we all change our actions towards what we
+            can have a positive impact on, the world would be a better place
+            tomorrow.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            We declare that respect is the basis of our communication and action
+          </h3>
+          <p class="home_tf">
+            We declare that respect instead of fear or ego will be the basis of
+            our communication and our action in this digital nation. We don’t
+            have to agree but respect and tolerance for other opinions is a
+            must. We need to respect time, e.g. time of others and our own.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            We declare to respect the world’s finite resources
+          </h3>
+          <p class="home_tf">
+            We need to be respectful for finite world resources. We will
+            actively look to purchase products which are more sustainable. We
+            declare that we want to produce high quality and sustainable
+            products or services.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            We declare to produce fair and honest information
+          </h3>
+          <p class="home_tf">
+            We need to be respectful for finite world resources. We will
+            actively look to purchase products which are more sustainable. We
+            declare that we want to produce high quality and sustainable
+            products or services.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">We agree that no-one should ever be a product</h3>
+          <p class="home_tf">
+            We have become a product of a big data machine. Every action we take
+            on social media, Internet searches, etc, is potential profit for
+            someone else.
+            <br />
+            We should stop believing that we are getting these services for
+            free. This leads to manipulation and too much a consumption of our
+            world’s resources. This means we accept to pay for resources used.
+          </p>
+        </div>
+      </div>
+
+      <div class="ff-laws my-5">
+        <h2 class="h2 mx-auto text-uppercase my-3 font-weight-bold">
+          Digital Nation information laws
+        </h2>
+        <p class="home_tf">
+          The following laws are the fundamental information rights of our new
+          internet.
+        </p>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">Information is owned by the author(s)</h3>
+          <p class="home_tf">
+            We have sole ownership of our created information/content (e.g.
+            photos, text, videos).
+            <br />
+            “We” can be an organization (company, government, etc) or a person.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            Everyone has control over his or her own information and community
+            circles
+          </h3>
+          <p class="home_tf">
+            We define the circles (groups of people) we want to communicate
+            with.
+            <br />
+            We have absolute freedom to decide what we want to share, and with
+            whom we want to share it.
+            <br />
+            We can at any point in time retrieve/destroy our information or
+            revoke access rights.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            Information cannot be changed or read without the consent of the
+            author(s)
+          </h3>
+          <p class="home_tf">
+            No one has access to our information unless we have given explicit
+            access rights. It should be technically impossible for a hacker or
+            other organization to change information and represent it in another
+            way than originally intended.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">Information is neutral and can be verified</h3>
+          <p class="home_tf">
+            We can ask for verification of the information at any point in time.
+            <br />
+            We have the ability to search and consume information in freedom and
+            neutrality.
+            <br />
+            We don’t like that an AI (Artificial Intelligence) machine decides
+            what information gets priority or is blocked. Today we navigate in a
+            restricted information world, much smaller than we believe. Keeping
+            us uninformed and manipulating the information which is being
+            presented to us can influence our consumption and voting actions.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">Exchange of value should be a free</h3>
+          <p class="home_tf">
+            The act of exchanging anything should be free of charge and not
+            controlled by anyone. Anything can be exchanged, everywhere by
+            everyone.
+          </p>
+        </div>
+
+        <div class="data my-4">
+          <h3 class="h4 my-2">
+            Law enforcement access to information is important but should not be
+            abused
+          </h3>
+          <p class="home_tf">
+            Safety is important. Governments or law enforcement agencies should
+            be able to access information when required in all transparency
+            (means authors know what rights governments have and when exercised)
+            and for the right reason.
+            <br />
+            This access should never be abused for other reasons as public
+            safety. This right should not lead to unnecessary censorship.
+          </p>
+        </div>
+
       </div>
     </article>
   </div>
