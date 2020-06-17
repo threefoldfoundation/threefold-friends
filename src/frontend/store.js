dd7f58c2b4b6e1406fc9d3ea34147ca09fa90a1a
hamdy <hamdy.a.farag@gmail.com>
2020-06-17 11:58:21 +0200

    relationships

--- src/frontend/store.js
+++ src/frontend/store.js
@@ -5,6 +5,7 @@ let fetched_projects = [];
 let project_tags = []
 let user_tags = []
 let all_tags = []
+let all_related = {}
 
 function compare(a, b) {
     if (a.info.rank > b.info.rank) return -1;
@@ -20,24 +21,56 @@ async function fetch_data(){
     for (var p of obj.projects) {
         p.name = p.info.name
     }
-    return { projects: obj.projects.sort(compare), users: obj.people.filter(user =>user.ecosystem.memberships.includes('ambassador')) };
+    return { projects: obj.projects.sort(compare), users: obj.people.filter(user =>user.ecosystem.memberships.includes('ambassador')||user.ecosystem.memberships.includes('employee')||user.ecosystem.memberships.includes('contributor')) };
 }
 
 fetch_data().then((data)=>{
     fetched_users = data['users']
     fetched_projects = data['projects']
-    console.log(fetched_projects)
+
     users.set(fetched_users)
     projects.set(fetched_projects)
     loading.set(false)
 
+    var relations = {}
+
     data['projects'].map(function(p){
         p.ecosystem.categories.forEach(function(item){
+            relations[p.name] = []
             if (! project_tags.includes(item)){
                 project_tags.push(item)
             }
+
+            if (!all_related[item])
+                all_related[item] = []
+            
+            all_related[item].push(p)
         })
     })
+
+    
+
+    // prop is relation
+    for (const prop in all_related) {
+        var related_circles = all_related[prop]
+
+        for(var circlename in relations){
+            for (var c of related_circles){
+                if (c.name == circlename)
+                    continue;
+                var idx = -1
+                for (var i=0; i < relations[circlename].length; i++){
+                    if(relations[circlename][i]['name'] == c.name)
+                        idx = i
+                }
+
+                if (idx == -1)
+                    relations[circlename].push({'name': c.name, 'relations': [prop]})
+                else
+                    relations[circlename][idx]['relations'].push(prop)
+            }
+        }
+      }
 
     data['users'].map(function(u){
         u.ecosystem.memberships.forEach(function(item){
@@ -66,3 +99,4 @@ export let loading = writable(true)
 export const projectags = writable(project_tags)
 export const usertags = writable(user_tags)
 export const alltags = writable(all_tags)
+export const related = writable(relations)
