69337b040e9505467c865d95fe98435ce1f0df67
samaradel <samar@incubaid.com>
2020-06-18 10:29:30 +0200

    Update Home page content #14

--- src/frontend/src/routes/Home.svelte
+++ src/frontend/src/routes/Home.svelte
@@ -13,19 +13,18 @@
     <!-- Post -->
     <article class="container font-weight-normal">
       <div class="row">
-      <header class="text-center mx-auto mb-2">
-        <div class="title">
-          <h1 class="text-uppercase my-3">community</h1>
-          <p class="font-weight-normal ambassador_text">
-            Our community are people who support us on the journey to a better world.
-            <br>
-            Each of them believes in the values and mission of ThreeFold. They are proud Token Holders and all have a value-added network.
-          </p>
-          <p class="home_tf">
-            
-          </p>
-        </div>
-      </header>
+        <header class="text-center mx-auto mb-2">
+          <div class="title">
+            <h1 class="text-uppercase my-3">community</h1>
+            <p class="font-weight-normal ambassador_text">
+              Our community supports us on the journey to a better world.
+              <br />
+              Each of them believes in the values and mission of ThreeFold. They
+              are proud Token Holders and all have a value-added network.
+            </p>
+            <p class="home_tf" />
+          </div>
+        </header>
         <span class="image featured">
           <img src="images/ambassador_home.png" alt="" />
         </span>
